ac315102c85c7c39a9158b948bdd889011214402
Ramil Garipov <ramillion@ya.ru>
2017-02-04 23:01:56 +0300

    fix load external script only one time
    bug with open modals after 404 page. potentially can be other bugs.

--- components/head.js
+++ components/head.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import Head from 'next/head';
 
+const isBrowser = (function () {
+  try {
+    return !!document;
+  } catch (e) {
+    return false;
+  }
+})();
+
 export default ({head}) => {
   const title = head.title || '';
+  const scripts = [
+    "https://code.jquery.com/jquery-2.2.4.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/collageplus/0.3.3/extras/jquery.collageCaption.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/collageplus/0.3.3/jquery.collagePlus.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/collageplus/0.3.3/extras/jquery.removeWhitespace.min.js",
+    "/static/js/modal.js",
+  ];
+
+  if (isBrowser) {
+    if (document._scriptsLoaded) {
+      scripts.length = 0;
+    }
+    document._scriptsLoaded = true;
+  }
+
   return (
     <Head>
       <title>{title + (title ? ' | ' : '') + 'Love Canvas'}</title>
       <link href="/static/css/main.css" rel="stylesheet"/>
-      <script src="https://code.jquery.com/jquery-2.2.4.min.js"/>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/collageplus/0.3.3/extras/jquery.collageCaption.min.js"/>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/collageplus/0.3.3/jquery.collagePlus.min.js"/>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/collageplus/0.3.3/extras/jquery.removeWhitespace.min.js"/>
-      <script src="/static/js/modal.js"/>
+      {scripts.map((src, i)=> <script key={i} src={src}/>)}
     </Head>
   );
 }
